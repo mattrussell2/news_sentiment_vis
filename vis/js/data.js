d218c536cafefed6c5dc11b56710fd4ac35935eb
Charles Winston <charles.winston@tufts.edu>
2018-11-27 10:40:56 -0500

    visualization starting

---
+++ vis/js/data.js
@@ -0,0 +1,58 @@
+var data = {
+  entity: {
+    name: "Donald Trump",
+    id: "1234"
+  },
+  source_sentiment: [
+    {
+      source: {
+        name: "CNN",
+        id: "cnn"
+      },
+      sentiment: {
+        score: -0.1,
+        magnitude: 0.5
+      }
+    },
+    {
+      source: {
+        name: "Fox News",
+        id: "fox-news"
+      },
+      sentiment: {
+        score: 0.1,
+        magnitude: 0.5
+      }
+    },
+    {
+      source: {
+        name: "Wall Street Journal",
+        id: "wsj"
+      },
+      sentiment: {
+        score: -0.1,
+        magnitude: 0.8
+      }
+    },
+    {
+      source: {
+        name: "MSNBC",
+        id: "msnbc"
+      },
+      sentiment: {
+        score: -0.5,
+        magnitude: 0.6
+      }
+    },
+    {
+      source: {
+        name: "Breitbart",
+        id: "bbn"
+      },
+      sentiment: {
+        score: 0.5,
+        magnitude: 0.9
+      }
+    }
+  ]
+}
