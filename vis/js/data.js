4937adde49cf1739aba92d7b32971270ab396aa1
Charles Winston <charles.winston@tufts.edu>
2018-11-29 10:36:41 -0500

    some vis work

--- vis/js/data.js
+++ vis/js/data.js
@@ -9,50 +9,80 @@ var data = {
         name: "CNN",
         id: "cnn"
       },
-      sentiment: {
-        score: -0.1,
-        magnitude: 0.5
-      }
+      pos_sentiment: {
+        score: 0.1,
+        magnitude: 0.5,
+        salience: 12
+      },
+      neg_sentiment: {
+        score: -0.5,
+        magnitude: 0.5,
+        salience: 100
+      },
     },
     {
       source: {
         name: "Fox News",
         id: "fox-news"
       },
-      sentiment: {
-        score: 0.1,
-        magnitude: 0.5
-      }
+      pos_sentiment: {
+        score: 0.3,
+        magnitude: 0.5,
+        salience: 40
+      },
+      neg_sentiment: {
+        score: -0.3,
+        magnitude: 0.5,
+        salience: 68
+      },
     },
     {
       source: {
-        name: "Wall Street Journal",
+        name: "WSJ",
         id: "wsj"
       },
-      sentiment: {
+      pos_sentiment: {
+        score: 0.1,
+        magnitude: 0.1,
+        salience: 13
+      },
+      neg_sentiment: {
         score: -0.1,
-        magnitude: 0.8
-      }
+        magnitude: 0.7,
+        salience: 40
+      },
     },
     {
       source: {
         name: "MSNBC",
         id: "msnbc"
       },
-      sentiment: {
-        score: -0.5,
-        magnitude: 0.6
-      }
+      pos_sentiment: {
+        score: 0.2,
+        magnitude: 0.3,
+        salience: 8
+      },
+      neg_sentiment: {
+        score: -0.7,
+        magnitude: 0.4,
+        salience: 115
+      },
     },
     {
       source: {
         name: "Breitbart",
         id: "bbn"
       },
-      sentiment: {
-        score: 0.5,
-        magnitude: 0.9
-      }
+      pos_sentiment: {
+        score: 0.6,
+        magnitude: 0.2,
+        salience: 70
+      },
+      neg_sentiment: {
+        score: -0.3,
+        magnitude: 0.6,
+        salience: 30
+      },
     }
   ]
 }
