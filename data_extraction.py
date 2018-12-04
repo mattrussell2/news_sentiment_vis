c6c85f9994e7e1c389fdd7b89a0cb28fad735bc0
Charles Winston <charles.winston@tufts.edu>
2018-12-03 21:27:33 -0500

    new data set

--- data_extraction.py
+++ data_extraction.py
@@ -12,36 +12,22 @@ from queries import *
 
 def dump_top_entities():
     db = DB(local=True)
-    entities_we_want = ["U.S.",
-                "Donald Trump",
-                "Democrats",
-                "Republican",
-                "Facebook",
-                "White House",
-                "Congress",
-                "Florida",
-                "China",
-                "Amazon",
-                "Russia",
-                "Saudi",
-                "Khashoggi"]
     result = []
-    for entity in db.get_top_entities(50):
-        if entity["name"] in entities_we_want:
-            result.append(entity)
+    for entity in db.get_top_entities(500):
+        result.append(entity)
 
-    json.dump(result, open("top_entities.json", "w"))
+    json.dump(result, open("top500_entities.json", "w"))
 
 def dump_sources():
     db = DB(local=True)
     json.dump(db.get_sources(), open("sources.json", "w"))
 
-def dump_entity_sentiment():
+def dump_entity_sentiment(n):
     sources = json.load(open("sources.json", "r"))
-    entities = json.load(open("top_entities.json", "r"))
+    entities = json.load(open("top{}_entities.json".format(n), "r"))
 
     db = DB(local=True)
     json.dump(db.get_entity_sentiment_with_time(entities, sources), open("dataset_with_time.json", "w"))
 
 dump_top_entities()
-dump_entity_sentiment()
+dump_entity_sentiment(500)
